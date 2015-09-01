7658f4a95fce25012d3ebd19d78c52151f67af7a
monicalemos <monica.lemos90@gmail.com>
2015-09-01 20:21:07 +0100

    Login Pages

---
+++ MemProjectApp/src/main/java/com/mem/app/model/enumerates/TipoUtilizador.java
@@ -0,0 +1,7 @@
+package com.mem.app.model.enumerates;
+
+public enum TipoUtilizador {
+
+	TECNICO, CUIDADOR;
+	
+}
---
+++ MemProjectApp/src/main/java/com/mem/app/model/repository/memory/UtilizadorRepository.java
@@ -0,0 +1,79 @@
+package com.mem.app.model.repository.memory;
+
+import java.util.LinkedList;
+
+import com.mem.app.dao.impl.UtilizadorDAOImpl;
+import com.mem.app.model.Utilizador;
+import com.mem.app.model.repository.IRepository;
+
+public class UtilizadorRepository implements IRepository<Utilizador> {
+
+	private LinkedList<Utilizador> repository;
+	private UtilizadorDAOImpl utilImpl = new UtilizadorDAOImpl();
+
+	private Utilizador getNewUtilizador() {
+		
+		Utilizador utilizador = new Utilizador();
+		utilizador.setIdUtilizador(1);
+		utilizador.setNomeUtilizador("admin");
+		utilizador.setPassword("admin");
+		utilizador.setTipoUtilizador("Utilizador");
+		utilizador.setEmail("adminEmail");
+
+		return utilizador;
+	}
+
+	public UtilizadorRepository() {
+				repository = new LinkedList<Utilizador>();
+				repository.add(getNewUtilizador()); 
+			}
+
+	@Override
+	public Iterable<Utilizador> getAll() {
+		// TODO Auto-generated method stub
+		return repository;
+	}
+
+	@Override
+	public Iterable<Utilizador> getAllPaged(int currentpage, int nextNbrItems) {
+		// TODO Auto-generated method stub
+		return repository;
+	}
+	@Override
+	public Utilizador select(int id) {
+		// TODO Auto-generated method stub
+		return repository.getFirst();
+	}
+	
+	@Override
+	public Utilizador selectObject(Utilizador utilizador) {
+		
+		String username = utilizador.getNomeUtilizador();
+		String password = utilizador.getPassword();
+		
+		
+		// TODO Auto-generated method stub
+		return repository.getFirst();
+	}
+
+	@Override
+	public boolean update(Utilizador obj) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean create(Utilizador obj) {
+
+		System.out.println(obj.getEmail() + " - " + obj.getNomeUtilizador() + " - "
+				+ obj.getPassword() + obj.getTipoUtilizador());
+		// TODO Auto-generated method stub
+		return repository.add(obj);
+	}
+
+	@Override
+	public boolean delete(int id) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+}
